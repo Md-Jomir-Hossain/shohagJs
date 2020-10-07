2186b477a8903c8ee8d759512ad51c46d1171822
MD Jomir Hossain <45014173+Md-Jomir-Hossain@users.noreply.github.com>
2020-10-07 11:59:20 +0600

    Add files via upload

---
+++ Shohag JS/main.js
@@ -0,0 +1,41 @@
+// first work
+// function date() {
+//     document.getElementById("date").innerHTML = Date();
+// }
+
+// 2nd work
+// function lightOn() {
+//   document.getElementById("img").src = "/pic_bulbon.gif";
+// }
+
+// function lightOff() {
+//   document.getElementById("img").src = "/pic_bulboff.gif";
+// }
+
+
+// one more example
+// function day() {
+//     document.getElementById("images").src = "/nature-3134705_640.jpg";
+// }
+// function night() {
+//     document.getElementById("images").src = "/milky-way-984050_640.jpg";
+// }
+
+
+
+
+// 3rd work
+// function dNone() {
+//     document.getElementById("img").style.display = "none";
+// }
+// function dBlock() {
+//     document.getElementById("img").style.display = "block";
+// }
+
+// one more example
+// function dHidden() {
+//     document.getElementById("img").style.visibility = "hidden";
+// }
+// function dVisibility() {
+//     document.getElementById("img").style.visibility = "visible";
+// }
